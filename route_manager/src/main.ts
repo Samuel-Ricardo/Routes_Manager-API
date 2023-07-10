475d6499b70fc96083a27adbb2d8114a64b7875f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-10 11:39:35 -0300

    [:gear:] | setup: swagger (docs)

--- route_manager/src/main.ts
+++ route_manager/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Route Manager')
+    .setDescription('The Route Manager API description')
+    .setVersion('1.0')
+    .addTag('Route Manager')
+    .setContact(
+      'Route Manager - Repository',
+      'https://github.com/Samuel-Ricardo/Routes_Manager-API',
+      'samuelricardoofficial@gmail.com',
+    )
+    .setLicense(
+      'MIT',
+      'https://github.com/Samuel-Ricardo/Routes_Manager-API/blob/main/LICENSE',
+    )
+    .build();
+
   await app.listen(3000);
 }
 bootstrap();
