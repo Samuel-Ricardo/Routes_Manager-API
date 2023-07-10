29fe93e2e8bca6df05d20d773dc6ab00b7e9b6da
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-10 17:20:49 -0300

    [:sparkles:] | create: repository - route (TypeORM)

---
+++ route_manager/src/@core/infra/db/typeorm/route-typeorm.repository.ts
@@ -0,0 +1,15 @@
+import { Repository } from 'typeorm';
+import { RouteRepositoryInterface } from 'src/@core/domain/route.repository';
+import { Route } from 'src/@core/domain/route.entity';
+
+export class RouteTypeormRepository implements RouteRepositoryInterface {
+  constructor(private repository: Repository<Route>) {}
+
+  async insert(route: Route): Promise<void> {
+    this.repository.save(route);
+  }
+
+  async findAll(): Promise<Route[]> {
+    return this.repository.find();
+  }
+}
