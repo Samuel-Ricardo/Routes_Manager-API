7fa3d0ae95e01d32bd0b5d6603e87e436ffd4652
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-10 23:17:24 -0300

    [:sparkles:] | setup: module - routes

--- route_manager/src/routes/routes.module.ts
+++ route_manager/src/routes/routes.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
+import { TypeOrmModule, getDataSourceToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { RouteSchema } from '../@core/infra/db/typeorm/route.schema';
+import { RouteTypeormRepository } from '../@core/infra/db/typeorm/route-typeorm.repository';
+import { Route } from '../@core/domain/route.entity';
+import { RouteInMemoryRepository } from 'src/@core/infra/db/in-memory/route-in-memory.repository';
+import { CreateRouteUseCase } from 'src/@core/application/create-route.use-case';
+import { RouteRepositoryInterface } from 'src/@core/domain/route.repository';
+import { ListAllRoutesUseCase } from 'src/@core/application/list-all-routes.use-case';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RouteSchema])],
   controllers: [RoutesController],
-  providers: [RoutesService],
+  providers: [
+    RoutesService,
+
+    {
+      provide: RouteTypeormRepository,
+      useFactory: (datasource: DataSource) =>
+        new RouteTypeormRepository(datasource.getRepository(Route)),
+      inject: [getDataSourceToken()],
+    },
+    {
+      provide: RouteInMemoryRepository,
+      useClass: RouteInMemoryRepository,
+    },
+    {
+      provide: CreateRouteUseCase,
+      useFactory: (repository: RouteRepositoryInterface) =>
+        new CreateRouteUseCase(repository),
+      inject: [RouteTypeormRepository],
+    },
+    {
+      provide: ListAllRoutesUseCase,
+      useFactory: (repository: RouteRepositoryInterface) =>
+        new ListAllRoutesUseCase(repository),
+      inject: [RouteTypeormRepository],
+    },
+  ],
 })
 export class RoutesModule {}
